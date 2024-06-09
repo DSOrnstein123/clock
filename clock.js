0860a0218a64b958bf4a0d32ed5a41effd72b9a3
DSOrnstein <namphonggithub@gmail.com>
2024-06-09 19:12:54 +0700

    Make showTime() function

---
+++ clock.js
@@ -0,0 +1,28 @@
+function showTime() {
+  const now = new Date();
+
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  let session = "";
+
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    hour -= 12;
+    session = "PM";
+  } else {
+    session = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+
+  document.getElementById(
+    "MyClockDisplay"
+  ).innerHTML = `${hour}:${minute}:${second} ${session}`;
+}
